dc27935c019fd4ea52a9ef096768e63d780d1998
Super <minchao.dong@melot.cn>
2018-01-21 14:28:38 +0800

    feature: 除去borader

--- PictureBanner/AppDelegate.m
+++ PictureBanner/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    ViewController *rootVC = [[ViewController alloc]init];
+    self.window = [[UIWindow alloc]initWithFrame:[[UIScreen mainScreen]bounds]];
+    [self.window setRootViewController:rootVC];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
